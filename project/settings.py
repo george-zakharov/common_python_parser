87d3fea3b7683f7264fd069478a689ac6553b081
George Zakharov <g.zaharov@cbonds.info>
2021-01-09 03:26:54 +0300

    Add test blog app

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project.apps.blog',  # TODO: JUST FOR TEST
 ]
 
 MIDDLEWARE = [
